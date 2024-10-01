3a19fa5d42c6fceb512a0a48913e60616b710960
TudorNanuTMTA11 <tudornanu97@gmail.com>
2024-10-01 15:06:26 +0300

    environment

--- BDD/environment.py
+++ BDD/environment.py
@@ -1,8 +1,11 @@
-from BDD.browser import Browser
-
+from browser import Browser
+from Pages.inventory_page import Inventory_page
+from Pages.main_page import Main_page
 def before_all(context):
     context.browser = Browser()
     context.browser.maximise_windows()
+    context.inventory_page = Inventory_page
+    context.main_page = Main_page
 
 def after_all(context):
     context.browser = Browser()
