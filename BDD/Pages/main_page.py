98af464cbb5fef5f21f8b85bc298b691bd790975
TudorNanuTMTA11 <tudornanu97@gmail.com>
2024-10-10 12:42:16 +0300

    main_page

--- BDD/Pages/main_page.py
+++ BDD/Pages/main_page.py
@@ -13,11 +13,26 @@ class Main_page(Browser):
     INVENTORY_ITEM_PRICE = (By.XPATH, '//*[@class = "inventory_item_price"]')
     INVENTORY_ITEM_NAME = (By.XPATH, '//*[@class = "inventory_item_name"]')
     ADD_TO_CART_BTN = (By.XPATH, '//button[contains(text(),"Add to cart")]')
+    SHOPPING_CART_LINK = (By.CLASS_NAME, 'shopping_cart_link')
     REMOVE_SAUCE_LABS_BACKPACK_BTN_INPUT = (By.XPATH, '//button[@class="btn btn_secondary btn_small btn_inventory')
     SWAG_LABS_LOGO = (By.XPATH, '//*[@class = "app_logo"]')
+    CHECKOUT_BTN = (By.ID, 'checkout')
+    FIRST_NAME = (By.ID, 'first-name')
+    LAST_NAME = (By.ID, 'last-name')
+    POSTAL_CODE = (By.ID, 'postal-code')
+    CONTINUE_BTN = (By.ID, 'continue')
+    FINISH_BTN = (By.ID, 'finish')
+    ORDER_COMPLETE_MESSAGE = (By.XPATH, '//div[@id="checkout_complete_container"]//h2')
+    REMOVE_PRODUCT = (By.ID, 'remove-sauce-labs-backpack')
+    ERROR_FIRST_NAME_MESSAGE = (By.XPATH, '//*[@class = "error-message-container error"]')
+    ERROR_LAST_NAME_MESSAGE = (By.XPATH, '//*[@class="error-message-container error"]')
+    ERROR_POSTAL_CODE_MESSAGE = (By.XPATH, '//*[@class="error-message-container error"]')
+    CONTINUE_SHOPPING_BTN = (By.ID, 'continue-shopping')
+    CANCEL_BTN = (By.ID, 'cancel')
+    BACK_TO_PRODUCTS = (By.ID, 'back-to-products')
 
 
-def i_click_on_the_sort_button(self):
+    def i_click_on_the_sort_button(self):
         try:
             sort_items = self.chrome.find_element(*self.PRODUCT_SORT_CONTAINER)
             sort_items.click()
@@ -89,6 +104,13 @@ def i_click_on_the_sort_button(self):
         except Exception as e:
             logging.error(f'An error occurred while showing the names from Z to A : {str(e)}')
 
+    def the_logo_is_displayed(self):
+        try:
+            logo = WebDriverWait(self.chrome, 10).until(EC.visibility_of_element_located(self.SWAG_LABS_LOGO))
+            assert logo.is_displayed()
+            logging.info('The logo is displayed')
+        except Exception as l:
+            logging.error(f"An error occurred while displaying the logo website : {str(l)}")
 
     def i_click_on_the_add_to_cart_button(self):
         try:
@@ -106,6 +128,7 @@ def i_click_on_the_sort_button(self):
         except Exception as e:
             logging.error(f'An error occurred while removing the item from cart : {str(e)}')
 
+
     def the_product_is_removed_from_cart(self):
         try:
             sort_items_1 = self.chrome.find_element(*self.REMOVE_SAUCE_LABS_BACKPACK_BTN_INPUT)
@@ -114,9 +137,134 @@ def i_click_on_the_sort_button(self):
         except Exception as e:
             logging.error(f'An error occurred while showing the change on the website : {str(e)}')
 
-    def the_logo_is_displayed(self):
+    def the_product_is_added_to_cart(self):
         try:
-            logo = WebDriverWait(self.chrome, 10).until(EC.visibility_of_element_located(self.SW)
+            added_to_cart = self.chrome.find_element(*self.SHOPPING_CART_LINK)
+            added_to_cart.click()
+            logging.info('The user is redirected to cart page')
+        except Exception as t:
+            logging.error(f'An error occurred while being redirected to the cart page : {str(t)}')
+
+    def i_click_on_the_checkout_button(self):
+        try:
+            checkout_btn = self.chrome.find_element(*self.CHECKOUT_BTN)
+            checkout_btn.click()
+            logging.info('The user is redirecting to the details page')
+        except Exception as e:
+            logging.error(f'An error occurred while being redirected to the details page : {str(e)}')
+    def the_first_name_is_inserted(self):
+        try:
+            first_name = self.chrome.find_element(*self.FIRST_NAME)
+            first_name.send_keys()
+            logging.info('The first name is inserted')
+        except Exception as t:
+            logging.error(f'An error occurred while inserting the first name : {str(t)}')
+
+    def the_last_name_is_inserted(self):
+        try:
+            last_name = self.chrome.find_element(*self.LAST_NAME)
+            last_name.send_keys()
+            logging.info('The last name is inserted')
+        except Exception as t:
+            logging.error(f'An error occurred while inserting the last name : {str(t)}')
+
+    def the_postal_code_is_inserted(self):
+        try:
+            postal_code = self.chrome.find_element(*self.POSTAL_CODE)
+            postal_code.send_keys()
+            logging.info('The postal code is inserted')
+        except Exception as t:
+            logging.error(f'An error occurred while inserting the postal code : {str(t)}')
+
+    def i_click_on_the_continue_button(self):
+        try:
+            continue_btn = self.chrome.find_element(*self.CONTINUE_BTN)
+            continue_btn.click()
+            logging.info('The shopping procedure continues')
+        except Exception as e:
+            logging.error(f'An error occurred while clicking the continue button : {str(e)}')
+
+    def i_click_on_the_finish_button(self):
+        try:
+            finish_btn = self.chrome.find_element(*self.FINISH_BTN)
+            finish_btn.click()
+            logging.info('The shopping procedure is finished')
+        except Exception as e:
+            logging.error(f'An error occurred while finishing the shopping procedure: {str(e)}')
+
+    def the_message_order_complete_is_shown(self):
+        try:
+            order_complete = (WebDriverWait(self.chrome, 10).
+                              until(EC.visibility_of_element_located(self.ORDER_COMPLETE_MESSAGE)))
+            assert order_complete.is_displayed()
+            logging.info('The order complete message is displayed')
+        except Exception as l:
+            logging.error(f"An error occurred while displaying the order complete message : {str(l)}")
+
+    def i_click_on_the_back_to_products_button(self):
+        try:
+            back_to_products_btn = self.chrome.find_element(*self.BACK_TO_PRODUCTS)
+            back_to_products_btn.click()
+            logging.info('The user is redirected back to the main page')
+        except Exception as e:
+            logging.error(f'An error occurred while being redirected back to the main page: {str(e)}')
+
+    def i_click_on_the_remove_product_button(self):
+        try:
+            remove_product_btn = self.chrome.find_element(*self.REMOVE_PRODUCT)
+            remove_product_btn.click()
+            logging.info('The product is removed')
+        except Exception as e:
+            logging.error(f'An error occurred while removing the product from cart: {str(e)}')
+
+    def i_click_on_the_continue_shopping_button(self):
+        try:
+            continue_shopping_btn = self.chrome.find_element(*self.CONTINUE_SHOPPING_BTN)
+            continue_shopping_btn.click()
+            logging.info('The shopping procedure is continued')
+        except Exception as e:
+            logging.error(f'An error occurred while continuing the shopping procedure: {str(e)}')
+
+
+    def i_click_on_the_cancel_button(self):
+        try:
+            cancel_btn = self.chrome.find_element(*self.CANCEL_BTN)
+            cancel_btn.click()
+            logging.info('Buying procedure is cancelled')
+        except Exception as e:
+            logging.error(f'An error occurred while cancelling the buying procedure {str(e)}')
+
+    def the_message_error_first_name_is_required_is_shown(self):
+        try:
+            error_first_name = (WebDriverWait(self.chrome, 10).
+                              until(EC.visibility_of_element_located(self.ERROR_FIRST_NAME_MESSAGE)))
+            assert error_first_name.is_displayed()
+            logging.info('The error message first name is required is displayed')
+        except Exception as l:
+            logging.error(f"An error occurred while displaying the error message first name is required : {str(l)}")
+
+    def the_message_error_last_name_is_required_is_shown(self):
+        try:
+            error_last_name = (WebDriverWait(self.chrome, 10).
+                              until(EC.visibility_of_element_located(self.ERROR_LAST_NAME_MESSAGE)))
+            assert error_last_name.is_displayed()
+            logging.info('The error message last name is required is displayed')
+        except Exception as l:
+            logging.error(f"An error occurred while displaying the error message last name is required : {str(l)}")
+
+    def the_message_error_postal_code_is_required_is_shown(self):
+        try:
+            error_postal_code = (WebDriverWait(self.chrome, 10).
+                              until(EC.visibility_of_element_located(self.ERROR_POSTAL_CODE_MESSAGE)))
+            assert error_postal_code.is_displayed()
+            logging.info('The error message postal code is required is displayed')
+        except Exception as l:
+            logging.error(f"An error occurred while displaying the error message postal code is required : {str(l)}")
+
+
+
+
+
 
 
 
