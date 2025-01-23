fd0bead69679df643039ab14417904b955943be9
TudorNanuTMTA11 <tudornanu97@gmail.com>
2025-01-23 11:19:23 +0200

    saucedemo_login_button.feature

--- BDD/Pages/inventory_page.py
+++ BDD/Pages/inventory_page.py
@@ -1,5 +1,3 @@
-from time import sleep
-
 from selenium.common import NoAlertPresentException
 from selenium.webdriver.common.by import By
 
@@ -12,41 +10,25 @@ class Inventory_page(Browser):
     LOGIN_BUTTON = (By.ID, "login-button")
     USER_NAME = (By.ID, "user-name")
     PASSWORD = (By.ID, "password")
+
     def open_inventory_page(self):
-        sleep(3)
         self.chrome.get("https://www.saucedemo.com/")
-
-
-
 
     def insert_username(self, username):
         try:
-            user_name= self.chrome.find_element(*self.USER_NAME)
-            user_name.send_keys(username)
+            user_name = self.chrome.find_element(*self.USER_NAME).send_keys(username)
         except Exception as i:
-            logging.error(f"An error occurred while inserting the username : {str(i)}")
-
-    def insert_invalid_password(self, password):
-        try:
-            user_password = self.chrome.find_element(self.PASSWORD)
-            user_password.send_keys(password)
-        except Exception as i:
-            logging.error(f"An error occurred while inserting the password : {str(i)}")
+            logging.error(f"An error occurred while using the username : {str(i)}")
 
     def click_login_button(self):
-        max_try = 3
-        attempts = 0
-        while attempts < max_try:
-            try:
-                login_button = self.chrome.find_element(*self.LOGIN_BUTTON)
-                if login_button:
-                    login_button.click()
-                    break
-                else:
-                    raise AssertionError('Login button element was not found')
-            except Exception as i:
-                logging.error(f"An error occurred while clicking the login button : {str(i)}")
-                attempts += 1
+        try:
+            login_button = self.chrome.find_element(*self.LOGIN_BUTTON)
+            if login_button:
+                login_button.click()
+            else:
+                raise AssertionError('Login button element was not found')
+        except Exception as i:
+            logging.error(f"An error occurred while clicking the login button : {str(i)}")
 
     def login_failed(self, error_message):
         try:
@@ -57,13 +39,14 @@ class Inventory_page(Browser):
         except NoAlertPresentException:
             assert False, "Expected alert not found"
 
-    def insert_password(self):
+    def insert_password(self, user_password):
         try:
-            userpassword = self.chrome.find_element(self.PASSWORD)
-            userpassword.send_keys("standard_sauce")
+            userpassword = self.chrome.find_element(*self.PASSWORD).send_keys(user_password)
         except Exception as i:
-            logging.error(f"An error occurred while inserting the password: {str(i)}")
+            logging.error(f"An error occurred while using the password: {str(i)}")
+
 
     def main_page(self):
         page_url = "https://www.saucedemo.com/inventory.html"
-        assert self.chrome.current_url == page_url
+        assert self.chrome.current_url == page_url, (f"An error occurred. Expected page_url {self.chrome.current_url}",
+                                                             f"Actual current_url {page_url}")
