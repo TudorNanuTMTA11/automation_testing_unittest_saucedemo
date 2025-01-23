09edb28409c1aa95f6d853c7d34ae3b337ab636e
TudorNanuTMTA11 <tudornanu97@gmail.com>
2025-01-23 11:20:03 +0200

    environment.py

--- BDD/environment.py
+++ BDD/environment.py
@@ -4,9 +4,8 @@ from Pages.main_page import Main_page
 def before_all(context):
     context.browser = Browser()
     context.browser.maximise_windows()
-    context.inventory_page = Inventory_page
-    context.main_page = Main_page
+    context.inventory_page = Inventory_page()
+    context.main_page = Main_page()
 
 def after_all(context):
-    context.browser = Browser()
     context.browser.close_browser()
